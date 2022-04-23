6cc598fe4e73edc94ea8098502a02cdf2e2187a2
Jesus Vallez Gomis <jesusvg91@gmail.com>
2022-04-23 18:09:16 +0200

    feature(init): init project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App(): JSX.Element {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-6xl mb-2 font-bold">Hi</h1>
+      <h2 className="text-2xl">React + TS + ESLint + Tailwind</h2>
+    </div>
+  )
+}
+
+export default App
