2a4dd34e38f7792f11ccd1201c0520f74f35778c
Jesus Vallez Gomis <jesusvg91@gmail.com>
2022-07-29 19:19:12 +0200

    feat(deps): use npm and update deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 function App(): JSX.Element {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-6xl mb-2 font-bold">Hi</h1>
+      <h1 className="text-6xl mb-2 font-bold">
+        Hi{' '}
+        <span role="img" aria-label="Waving hand">
+          👋
+        </span>
+      </h1>
       <h2 className="text-2xl">React + TS + ESLint + Tailwind</h2>
     </div>
   )
